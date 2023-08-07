fc90ef3b309abd080c13ad942646b1c66730dc47
Inuri92 <102139673+Inuri92@users.noreply.github.com>
2023-08-07 15:03:34 +0530

    Add files via upload

---
+++ QnA_App/Chatgpt_App.py
@@ -0,0 +1,11 @@
+# Q&A App for Drugs & Medication - Developed with OpenAI APIs
+# by Inuri Muthukumarana
+# Last Update: 2022-08-07
+# streamlit run F:\QnA_App\Chatgpt_App.py
+
+import streamlit as st
+
+st.header("Welcome to the Medication and Drugs Q&A app powered by OpenAI!")
+st.subheader("Inuri Muthukumarana")
+
+st.write("https://github.com/Inuri92/ChatgptCapstoneProject/")
